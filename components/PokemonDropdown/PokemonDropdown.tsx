8381e4dd8e4d0caf82cc6e28f6dde65b4f6c535d
Ben <benyoung1@hotmail.co.uk>
2022-01-14 09:20:15 +0000

    Add launch profile for debugging getserversideprops for initial load for dropdown Add a dropdown component

---
+++ components/PokemonDropdown/PokemonDropdown.tsx
@@ -0,0 +1,31 @@
+import { PokemonOption } from "../../models/PokemonOption";
+
+type PokemonDropdownProps = {
+    options: PokemonOption[]
+}
+
+const PokemonDropdown = ({ options }: PokemonDropdownProps) => {
+
+    return (
+        <select className="form-select appearance-none
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding bg-no-repeat
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+            <option selected>Choose a Pokemon</option>
+            {options.map(x => <option className="capitalize" key={x.name} value={x.name}>{x.name}</option>)}
+        </select>
+    );
+}
+
+export default PokemonDropdown;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <title>The Poke-Next app</title>
+        <meta name="description" content="App to search pokemons" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>);
 }
 
 export default MyApp
